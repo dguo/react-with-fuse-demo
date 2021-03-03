841b6be4fae1c1b9cc64ff9fd30eeae3f041cbdf
Danny Guo <dannyguo91@gmail.com>
2021-03-02 19:01:41 -0500

    Add the search functionality

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# react-with-fuse-demo
+# React With Fuse Demo
+
+Demo website to show how to use [Fuse.js](https://fusejs.io/) in a React
+application. The demo is available
+[here](https://www.dannyguo.com/react-with-fuse-demo).
+
+## License
+
+MIT
--- index.js
+++ index.js
@@ -1,8 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
+import Fuse from "fuse.js";
 
-function App() {
-  return <h1>Hello, world!</h1>;
+// Courtesy of https://github.com/dariusk/corpora
+import dogs from "./dogs.json";
+
+const fuse = new Fuse(dogs);
+
+function getSearchResultsWithBasicApproach(query) {
+  if (!query) {
+    return [];
+  }
+
+  return dogs.filter((dog) => dog.toLowerCase().includes(query.toLowerCase()));
 }
 
-ReactDom.render(<App />, document.getElementById("app"));
+function getSearchResultsWithFuse(query) {
+  if (!query) {
+    return [];
+  }
+
+  return fuse.search(query).map((result) => result.item);
+}
+
+function SearchResults(props) {
+  if (!props.results) {
+    return null;
+  }
+
+  if (!props.results.length) {
+    return <p>There are no results for your query.</p>;
+  }
+
+  return (
+    <ol>
+      {props.results.map((result) => (
+        <li key={result}>{result}</li>
+      ))}
+    </ol>
+  );
+}
+
+function Search() {
+  const [searchResults, setSearchResults] = useState(null);
+
+  return (
+    <>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const query = event.target.elements.dogBreedSearch.value;
+          const useFuse = event.target.elements.fuse.checked;
+          setSearchResults(
+            useFuse
+              ? getSearchResultsWithFuse(query)
+              : getSearchResultsWithBasicApproach(query)
+          );
+        }}
+      >
+        <label htmlFor="dogBreedSearch">Search for a dog dog:</label>
+        <input type="search" id="dogBreedSearch" />
+        <input type="checkbox" name="fuse" />
+        <label htmlFor="fuse"> Use Fuse.js</label>
+        <br />
+        <button>Search</button>
+      </form>
+
+      <SearchResults results={searchResults} />
+    </>
+  );
+}
+
+ReactDom.render(<Search />, document.getElementById("app"));
